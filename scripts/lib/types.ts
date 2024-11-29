bed4855045a60df8c448c1024f991d69377e8c8f
yodas7 <173234870+yodas7@users.noreply.github.com>
2024-11-29 09:39:50 +0900

    DDS-1513 Publish token JSONs (#32)

--- scripts/lib/types.ts
+++ scripts/lib/types.ts
@@ -6,8 +6,10 @@ export interface Theme {
   readonly selectedTokenSets: Readonly<Record<string, "enabled" | "disabled">>;
 }
 
+export type TokenValue = string | number;
+
 /** Record<tokenName, tokenValue> */
-export type Tokenset = Record<string, string>;
+export type Tokenset = Record<string, [type: string, value: TokenValue]>;
 
 /** Record<themeName, Tokenset> */
 export type ThemeTokensetMap = Record<string, Tokenset>;
